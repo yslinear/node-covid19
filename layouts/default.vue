6e01e7401832adab0406fbc633a3ef395f83a715
Ying-Shan Lin <yslinear@gmail.com>
2022-05-08 01:48:37 +0800

    refactor: 修改版面

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,7 +26,7 @@
     </v-navigation-drawer>
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title v-text="title" />
+      <v-toolbar-title v-text="currentRouteTitle" />
       <v-spacer />
     </v-app-bar>
     <v-main>
@@ -34,18 +34,10 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <v-col class="text-center" cols="12">
+        {{ new Date().getFullYear() }} — <strong>yslinear.dev</strong>
+      </v-col>
     </v-footer>
   </v-app>
 </template>
@@ -60,21 +52,23 @@ export default {
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
+          icon: 'mdi-home-variant',
           title: '首頁',
           to: '/',
         },
         {
-          icon: 'mdi-medical-bag',
+          icon: 'mdi-test-tube',
           title: '快篩試劑查詢',
           to: '/fst',
         },
       ],
       miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'COVID 19',
     }
+  },
+  computed: {
+    currentRouteTitle() {
+      return this.$route.name
+    },
   },
 }
 </script>
